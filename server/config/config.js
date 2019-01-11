a1d168a4fbb702a8f8d739292cf1de20a365ade5
Shawn Doyle <shawndoyl@gmail.com>
2019-01-11 15:14:46 -0500

    Moved server files to separate repo

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-
-if(env === 'development' || env === 'testing') {
-	const config = require('./config.json')[env];
-	Object.keys(config).forEach(key => {
-		process.env[key] = config[key];
-	});
-}
